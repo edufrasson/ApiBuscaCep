9e7488491f2970db3427146262d5407122dc67d4
edufrasson <edudomine2016@gmail.com>
2023-02-13 09:34:27 -0300

    first commit

---
+++ Controller/Controller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+use FFI\Exception;
+
+abstract class Controller{
+
+    public static function getResponseAsJSON($data){
+        header("Access-Control-Allow-Origin: *");  
+        header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Origin, Content-Type, Accept');      
+        header("Content-type: application/json; charset=utf-8");
+        header("Cache-Control: no-cache, must-revalidate");
+        header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+        header("Pragma: public");      
+        
+        exit(json_encode($data));
+    }
+}
---
+++ Controller/EnderecoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+class EnderecoController extends Controller{
+    public static function index(){
+
+    }
+    public static function save(){
+
+    }
+    public static function getById(){
+        
+    }
+}
