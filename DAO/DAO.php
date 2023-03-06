611b826b7f2756ca999232d0269a15134de1a719
edufrasson <edudomine2016@gmail.com>
2023-03-06 08:35:53 -0300

    alterando arquivo dao

--- DAO/DAO.php
+++ DAO/DAO.php
@@ -1,13 +1,21 @@
 <?php
 
 namespace App\DAO;
+
 use PDO;
+use PDOException;
 
-abstract class DAO extends PDO{
+abstract class DAO extends PDO
+{
     public $conexao;
-    public function __construct(){
-        $dsn = "mysql:host=" . $_ENV['db']['host'] . ";dbname=" . $_ENV['db']['database'];
+    public function __construct()
+    {
+        try {
+            $dsn = "mysql:host=" . $_ENV['db']['host'] . ";dbname=" . $_ENV['db']['database'];
 
-        $this->conexao = new PDO($dsn, $_ENV['db']['user'], $_ENV['db']['pass'], array(PDO::MYSQL_ATTR_INIT_COMMAND =>  "SET NAMES UTF8"));
+            $this->conexao = new PDO($dsn, $_ENV['db']['user'], $_ENV['db']['pass'], array(PDO::MYSQL_ATTR_INIT_COMMAND =>  "SET NAMES utf8", PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        } catch (PDOException $e) {
+            echo 'erro: ' . $e->getMessage();
+        }
     }
 }
